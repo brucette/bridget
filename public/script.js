14b548cffb91c8621feb96e251664ce3113c3925
Tina Bruce <tinabruce42@hotmail.com>
2023-12-08 16:34:33 +0100

    Add favicon

--- public/script.js
+++ public/script.js
@@ -1,25 +1,20 @@
 const links = document.querySelectorAll(".filters a")
-console.log({links})
 
-links.forEach(item => item.addEventListener('click', (e) => filterProjects(e)))//filterProjects(e)))
+links.forEach(item => item.addEventListener('click', (e) => filterProjects(e)))
 
 function filterProjects(e) {
   // select all project divs
   const projects = document.querySelectorAll(".item");
-  console.log({projects})
+
   // Get the value to filter by in the event target's data-filter attribute
   let filter = e.target.dataset.filter;
-  console.log({filter})
-  
- if (filter === '*') { 
-  projects.forEach(project => project.classList.remove('hide'));
+  if (filter === '*') { 
+    projects.forEach(project => project.classList.remove('hide'));
  } else {
-   projects.forEach(project => { 
-    //console.log(project.classList)
-    //console.log(project.classList.contains(filter))
-    project.classList.contains(filter)
-    ? project.classList.remove('hide')
-    : project.classList.add('hide')
+    projects.forEach(project => { 
+      project.classList.contains(filter)
+      ? project.classList.remove('hide')
+      : project.classList.add('hide')
    });
   }
 }
